bc1e0f1208db4cc99295db389236931c61c21b59
Brayan Llanos <brayan0428@gmail.com>
2018-07-10 20:58:58 -0500

    Diseño ventana login y carpetas de demas vistas

---
+++ src/pages/periodos/periodos.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-periodos',
+  templateUrl: 'periodos.html'
+})
+export class PeriodosPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
